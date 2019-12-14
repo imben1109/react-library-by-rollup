382203a8bc5bcc30765a615d769b65a16080f48f
ben <imben1109@gmail.com>
2019-12-15 02:50:17 +1400

    update

---
+++ react-app-example1/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {Button} from 'react-library-rollup-example1';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+		<Button></Button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
